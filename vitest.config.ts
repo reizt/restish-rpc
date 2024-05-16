d00481a51439383a38d79ba07e72642f94dbdec2
Reiju Takahashi <reizt.dev@gmail.com>
2024-05-16 15:00:25 +0900

    Stop bundling

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,8 @@
-/// <reference types="vitest" />
-import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		globals: true,
 		include: ['test/**/*.test.ts'],
-	},
-	resolve: {
-		alias: {
-			'~': resolve(__dirname, './src'),
-		},
 	},
 });
